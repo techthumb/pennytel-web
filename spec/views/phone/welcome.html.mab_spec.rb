295bd6e399a277266c7057e66e3cd0aa1f3ab63e
Kunal Parikh <kunal@techthumb.in>
2009-09-03 17:08:58 +1000

    now with m|parkaby

--- spec/views/phone/welcome.html.mab_spec.rb
+++ spec/views/phone/welcome.html.mab_spec.rb
@@ -5,7 +5,6 @@ describe "/phone/welcome" do
     render 'phone/welcome'
   end
 
-  #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
     response.should have_tag 'p', %r[Find me in app/views/phone/welcome]
   end
