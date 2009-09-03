fc7e032865c0f4f12c6204e39313fb9699dfcb6b
Kunal Parikh <kunal@techthumb.in>
2009-09-03 15:20:09 +1000

    now with m|parkaby

--- spec/views/phone/welcome.html.mab_spec.rb
+++ spec/views/phone/welcome.html.mab_spec.rb
@@ -1,12 +1,12 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "/phone/welcome" do
-  before(:each) do
+  before :each  do
     render 'phone/welcome'
   end
 
   #Delete this example and add some real ones or delete this file
   it "should tell you where to find the file" do
-    response.should have_tag('p', %r[Find me in app/views/phone/welcome])
+    response.should have_tag 'p', %r[Find me in app/views/phone/welcome]
   end
 end
